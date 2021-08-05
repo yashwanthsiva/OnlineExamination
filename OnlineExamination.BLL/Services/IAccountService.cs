6b9bf4b46168442bcb3e8a340805c3541cbc556b
Yashwanth S <yashwanth.siva@aspiresys.com>
2021-08-05 09:31:44 +0530

    BLL

---
+++ OnlineExamination.BLL/Services/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineExamination.BLL.Services
+{
+    public interface IAccountService
+    {
+        Login
+    }
+}
